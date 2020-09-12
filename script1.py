11b90f7c7097f7ad4c45b623619666637dc4e242
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2020-09-12 10:25:32 +0530

    first commit

---
+++ script1.py
@@ -0,0 +1,14 @@
+import cv2
+
+s=input("Enter The Name of the File along with the Address: ")
+#print(s)
+a=int(input("Enter the Desired Breadth: "))
+b=int(input("Enter the Desired Height: "))
+t=input("Enter the Name of the New Resized File: ")
+t="Files/"+t
+s="Files/"+s
+img=cv2.imread(s,1)
+#cv2.imshow("Picture Box",img)
+#cv2.waitKey(0)
+resized_image=cv2.resize(img,(a,b))
+cv2.imwrite(t,resized_image)
